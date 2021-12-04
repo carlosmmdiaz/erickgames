ceac5c980bd484633f71da55879c384d8449941c
Carlos <carlosmmdiaz@gmail.com>
2021-12-04 13:05:28 +0100

    ci: 🎡 update rollup config to add favicons

--- rollup.config.js
+++ rollup.config.js
@@ -12,7 +12,20 @@ export default {
     nodeResolve(),
     terser(),
     copy({
-      assets: ["./packages/memory-game/assets", "./index.html"],
+      assets: [
+        "./packages/memory-game/assets",
+        "./index.html",
+        "./android-chrome-192x192.png",
+        "./android-chrome-512x512.png",
+        "./apple-touch-icon.png",
+        "./browserconfig.xml",
+        "./favicon-16x16.png",
+        "./favicon-32x32.png",
+        "./favicon.ico",
+        "./mstile-150x150.png",
+        "./safari-pinned-tab.svg",
+        "./site.webmanifest",
+      ],
     }),
   ],
 };
